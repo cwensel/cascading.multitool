71fbb7ff1064405763e630231b7b8de10560c683
Doc Ritezel <ritezel+github@gmail.com>
2011-05-10 14:53:54 -0700

    using ohrite/roundup branch for bash CI; added mt-test target to build.xml; moved include.sh to bourne syntax

--- src/test/mt/helper/include.sh
+++ src/test/mt/helper/include.sh
@@ -1,12 +1,16 @@
 # Copyright (c) 2011 Concurrent, Inc.
 
-if [[ "$MT_PATH" == "" ]]; then
-  MT_PATH=`dirname $(cd ${0%/*}/../../../.. && echo $PWD/${0##*/})`
+if [ -z "$MT_PATH" ]
+then
+  MT_PATH=`dirname $0`/../../../..
+  MT_PATH=`cd $MT_PATH && pwd`
 fi
 
-function include_dependencies () {
-  if [[ $# -gt 0 ]]; then
-    for dep in "$@"; do
+include_dependencies () {
+  if [ "$#" -gt "0" ]
+  then
+    for dep in "$@"
+    do
       . $MT_PATH/bin/functions/$dep.inc
     done
   fi
