ccd2d50b7aaa9e1491f5bce9d2172a7f87db25ec
Eric "Doc" Ritezel <eng@emimusic.com>
2011-05-09 18:28:56 -0700

    started install specs

---
+++ src/test/mt/install_test.sh
@@ -0,0 +1,22 @@
+# Copyright (c) 2011 Concurrent, Inc.
+
+. `dirname $(cd ${0%/*} && echo $PWD/${0##*/})`/include.sh
+
+describe "install.inc"
+
+before () {
+  include_dependencies install
+}
+
+it_displays_installation_usage () {
+  OUTPUT=`mt_install_usage`
+  test "$OUTPUT" != "" # uninteresting
+}
+
+it_sets_the_install_destination () {
+  test "$mt_install_destination" = ""
+  
+  mt_install_set_destination
+  
+  test "$mt_install_destination" = "$HOME/.multitool"
+}
