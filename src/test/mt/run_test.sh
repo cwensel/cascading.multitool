d32cdcc2aaffb86e13f6901abfff353111b08b28
Doc Ritezel <ritezel+github@gmail.com>
2011-05-12 14:16:06 -0700

    polish logging colorization even further

--- src/test/mt/run_test.sh
+++ src/test/mt/run_test.sh
@@ -5,7 +5,7 @@
 describe "run.inc"
 
 before () {
-  include_dependencies log run
+  color=always include_dependencies log run
 }
 
 it_detects_hadoop_if_HADOOP_HOME_is_set () {
@@ -19,7 +19,7 @@ it_exits_if_hadoop_is_not_in_HADOOP_HOME () {
   OUTPUT=`mt_run_detect_hadoop`
 
   ERROR_MESSAGE="HADOOP_HOME is set, but /var/bin/hadoop was not found."
-  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_reset_code $ERROR_MESSAGE$mt_log_reset_code"
+  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_clear $ERROR_MESSAGE$mt_log_clear"
 
   test "$OUTPUT" = "$ERROR_MESSAGE"
 }
@@ -31,7 +31,7 @@ it_exits_if_HADOOP_HOME_is_not_set() {
   OUTPUT=`mt_run_detect_hadoop`
 
   ERROR_MESSAGE="HADOOP_HOME was not set and hadoop is not in your PATH"
-  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_reset_code $ERROR_MESSAGE$mt_log_reset_code"
+  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_clear $ERROR_MESSAGE$mt_log_clear"
 
   test "$OUTPUT" = "$ERROR_MESSAGE"
 }
@@ -65,7 +65,7 @@ it_exits_if_multitool_jar_is_not_found () {
   OUTPUT=`mt_run`
 
   ERROR_MESSAGE="Could not find a multitool jar file in /some_mt_path"
-  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_reset_code $ERROR_MESSAGE$mt_log_reset_code"
+  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_clear $ERROR_MESSAGE$mt_log_clear"
 
   test "$OUTPUT" = "$ERROR_MESSAGE"
 }
@@ -77,7 +77,7 @@ it_exits_if_no_arguments_are_specified () {
   OUTPUT=`mt_run`
 
   ERROR_MESSAGE="No arguments specified"
-  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_reset_code $ERROR_MESSAGE$mt_log_reset_code"
+  ERROR_MESSAGE="${mt_log_red}ERROR$mt_log_clear $ERROR_MESSAGE$mt_log_clear"
 
   test "$OUTPUT" = "$ERROR_MESSAGE"
 }
