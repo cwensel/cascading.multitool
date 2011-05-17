36ea43cec8b420f8c272f78beb0cf86c7d70d79d
Doc Ritezel <ritezel+github@gmail.com>
2011-05-17 03:32:44 -0700

    add help tests

--- src/test/mt/help_test.sh
+++ src/test/mt/help_test.sh
@@ -3,3 +3,27 @@
 . `dirname $(cd ${0%/*} && echo $PWD/${0##*/})`/include.sh
 
 describe "help.inc"
+
+before () {
+  module_depends _route help
+}
+
+it_routes () {
+  tested=false
+  mt_help () {
+    tested=true
+  }
+  route_perform help
+  test "$tested" = "true"
+}
+
+it_has_usage () {
+  about=`module_annotate help about`
+  test "$about" = "display this screen"
+}
+
+it_formats_the_module_list () {
+  _MODULE_abouttest=testing
+  OUTPUT=`MODULES=test mt_help_module_list`
+  test "$OUTPUT" = "  test - testing"
+}
