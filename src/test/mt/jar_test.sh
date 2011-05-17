ca4c9d5b200127599e9e4d5aab580056ba5cb3ab
Doc Ritezel <ritezel+github@gmail.com>
2011-05-17 02:07:18 -0700

    WIP - attempt to fix tests

--- src/test/mt/jar_test.sh
+++ src/test/mt/jar_test.sh
@@ -4,9 +4,21 @@
 
 describe "jar.inc"
 
+before () {
+  TMP_HADOOP=`mktemp -d /tmp/mt-jar-hadoop-spec.XXXXXX`
+  mkdir -p $TMP_HADOOP/bin/
+  touch $TMP_HADOOP/bin/hadoop
+
+  module_depends hadoop
+}
+
+after () {
+  rm -rf $TMP_HADOOP
+}
+
 it_runs_silently_if_mt_jar_path_is_set () {
   mt_jar_path=/
-  OUTPUT=`module_depends jar`
+  OUTPUT=`PATH=$PATH:$TMP_HADOOP/bin module_depends jar`
   test "$OUTPUT" = ""
 }
 
@@ -16,7 +28,7 @@ it_runs_silently_if_it_finds_multitool_jar () {
   HERE_PATH=`dirname $(cd ${0%/*}/../../../.. && echo $PWD/${0##*/})`
   MT_PATH=.
 
-  OUTPUT=`cd $TMPDIR && . $HERE_PATH/bin/functions/jar.inc`
+  OUTPUT=`cd $TMPDIR && PATH=$PATH:$TMP_HADOOP/bin . $HERE_PATH/bin/functions/jar.inc`
 
   rm -rf $TMPDIR
   test "$OUTPUT" = ""
@@ -28,7 +40,7 @@ it_complains_if_it_cannot_find_multitool_jar () {
   HERE_PATH=`dirname $(cd ${0%/*}/../../../.. && echo $PWD/${0##*/})`
   MT_PATH=.
 
-  OUTPUT=`cd $TMPDIR && . $HERE_PATH/bin/functions/jar.inc`
+  OUTPUT=`cd $TMPDIR && PATH=$PATH:$TMP_HADOOP/bin . $HERE_PATH/bin/functions/jar.inc`
 
   rm -rf $TMPDIR
   test "$OUTPUT" = "ERROR multitool.jar not found"
