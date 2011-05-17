6cdb279ce21236ba6aa36ba00ac7c734247afec1
Doc Ritezel <ritezel+github@gmail.com>
2011-05-17 02:13:44 -0700

    fix up parameters test

--- src/test/mt/parameters_test.sh
+++ src/test/mt/parameters_test.sh
@@ -5,10 +5,17 @@
 describe "parameters.inc"
 
 before () {
-  module_depends parameters
+  TMP_HADOOP=`mktemp -d /tmp/mt-parameters-spec.XXXXXX`
+  mkdir -p $TMP_HADOOP/bin/
+  touch $TMP_HADOOP/bin/hadoop
+}
+
+after () {
+  rm -rf $TMP_HADOOP
 }
 
 it_prints_an_about_message () {
+  HADOOP_HOME=$TMP_HADOOP PATH=/usr/bin:/bin:/usr/sbin:/sbin module_depends parameters
   about=`module_annotate parameters about`
   test "$about" = "prints a description of a Cascading.Multitool parameters"
 }
